e67b3fdf38b097ea0b29b700c9b26c33825a4a85
Maxemeyer <maximilianmryka@gmail.com>
2025-11-28 14:27:17 +0100

    Update index.js
    - should minimize loading time

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const images = ["images/image1big.jpg", "images/image2big.jpg", "images/image3bi
 let imagesIndexNoLoop = 0;
 let imagesIndexLoop = 0;
 
+// preload Images for better performance
+images.forEach(img => {
+    new Image().src = img;
+});
+
 function changeBigPicture(url) {
     let bigPicture = document.getElementById("bigPicture");
     bigPicture.style.backgroundImage = "url('" + url + "')";
